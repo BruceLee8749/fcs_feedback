41aea2ca1c47495250b18185bcc883115221f823
zhaoqq <1q1q1q1q>
2022-10-14 16:27:15 +0800

    转换类型1、19、21、61、340

---
+++ Test/test_19_type.py
@@ -0,0 +1,31 @@
+import pytest
+from selenium.webdriver import Keys
+
+from main import BrowserAction
+from time import sleep
+import json
+from tools import *
+
+driver = BrowserAction()
+pro_path = os.path.dirname(os.path.dirname(__file__))
+path = get_conf('FCS', '测试结果文件夹')
+fcs_result_path = pro_path + "/" + path
+sheet_name = 'Sheet0'
+
+
+class TestCase:
+
+    def test_ZMYF_10058(self):
+        if get_cell(fcs_result_path, 35, 9, sheet_name) != '通过':
+            pytest.skip("url转换失败,不执行该case")
+        url = get_cell(fcs_result_path, 35, 10, sheet_name)
+        print(url)
+        driver.open_bro(url)
+        driver.click_ele("//span[@class='fcon ico_file_dir']")
+        sleep(2)
+        driver.click_ele("//span[@class='fcon ico_file_dir']")
+        driver.click_ele("//span[contains(text(),'.doc')]")
+        sleep(1)
+        driver.driver.switch_to.frame(driver.find_ele("//iframe[@class='pica-player pica-frame scroll']"))
+        driver.element_input("//input[@id='currentPage']", 41, clear=2)
+        driver.screenshot_save(35, sheet_name)
---
+++ Test/test_21_type.py
@@ -0,0 +1,41 @@
+import pytest
+from main import BrowserAction
+from time import sleep
+import json
+from tools import *
+
+driver = BrowserAction()
+pro_path = os.path.dirname(os.path.dirname(__file__))
+path = get_conf('FCS', '测试结果文件夹')
+fcs_result_path = pro_path + "/" + path
+sheet_name = 'Sheet0'
+
+
+class TestCase:
+
+    def test_ZMYF_8743(self):
+        """ZMYF-8743验证转换类型21下菜单栏是否会缩回"""
+        if get_cell(fcs_result_path, 3, 9, sheet_name) != '通过':
+            pytest.skip("url转换失败,不执行该case")
+        url = get_cell(fcs_result_path, 3, 10, sheet_name)
+        driver.open_bro(url)
+        driver.click_ele("//button[@title='文档管理']")
+        driver.click_ele("//button[@title='文档管理']")
+        sleep(2)
+        assert driver.get_attribute("//button[@title='文档管理']", "style") != "background-color: rgb(235, 232, 232);"
+
+    def test_ZMYF_9239(self):
+        """ZMYF-9329验证转换类型21下打印"""
+        if get_cell(fcs_result_path, 11, 9, sheet_name) != '通过':
+            pytest.skip("url转换失败,不执行该case")
+        url = get_cell(fcs_result_path, 11, 10, sheet_name)
+        driver.open_bro(url)
+        driver.click_ele("//input[@type='checkbox']")
+        sleep(1)
+        driver.click_ele("//*[text()=' 全选 ']")
+        sleep(1)
+        driver.click_ele("//*[text()='文件 ']")
+        sleep(1)
+        driver.click_ele("//*[text()=' 打印 ']")
+        sleep(1)
+        driver.screenshot_save(11, sheet_name)
